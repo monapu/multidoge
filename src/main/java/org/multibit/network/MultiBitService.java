ee890c6b79238ededd4e25f7b1e309a4df4594ce
monapu <mona@mona>
2014-04-17 22:34:59 +0900

    0.0.12

--- src/main/java/org/multibit/network/MultiBitService.java
+++ src/main/java/org/multibit/network/MultiBitService.java
@@ -221,9 +221,11 @@ public class MultiBitService {
 
     // Ensure there is a checkpoints file.
     File checkpointsFile = new File(checkpointsFilename);
-    //if (!checkpointsFile.exists()) {
+    if( checkpointsFile.length() > 6645 ) // とりあえずバグってるcheckpointsは消しとく
+        checkpointsFile.delete();
+    if (!checkpointsFile.exists()) {
       bitcoinController.getFileHandler().copyCheckpointsFromInstallationDirectory(checkpointsFilename);
-      //}
+    }
 
     // Use the larger of the installed checkpoints file and the user data checkpoint file (larger = more recent).
     ApplicationDataDirectoryLocator applicationDataDirectoryLocator = new ApplicationDataDirectoryLocator();
